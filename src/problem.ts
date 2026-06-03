fe80228369dffb585edf3caf4d4c6a8ad99ebd00
happydoggo <nguyenhaithanh26022009@gmail.com>
2026-06-03 15:31:05 +0700

    add reaction endpoint

--- src/problem.ts
+++ src/problem.ts
@@ -1,47 +1,53 @@
 import {z} from "zod";
-import Elysia from "elysia";
+import {Elysia,status} from "elysia";
 import { get_connection } from "./tools";
+import auth_middleware from "./auth_middleware";
 export const problem_route=new Elysia({prefix:"/problem"});
 problem_route.get('/home',async ()=>{
-     return await get_connection(async (db)=>{
+    return await get_connection(async (db)=>{
         return await db`select title,difficulty,reaction,id from problem`;
-     });
-     return [{title:"easy problem",difficulty:"easy",reaction:3,id:0},
-     {title:"medium problem",difficulty:"medium",reaction:-13,id:1},
-     {title:"hard problem",difficulty:"hard",reaction:26,id:2}];
+    });
 }
 ).get("/detail",async ({query,set})=>{
     set.headers["cache-control"]='public, s-maxage=3600, stale-while-revalidate=60';
     return await get_connection(async(db)=>{
-        return await db`select title,description,author_name as author,parameter,output 
+        return await db`select title,description,author_name as author,parameter,output,reaction 
         from problem where id=${query.problem_id}`;
     });
-    const data=[
-        {
-            title:"easy problem",
-            description:"some really long description here",
-            author:"phuc",
-            parameter:['x','y'],
-            output:[{"x":9,"y":3,"output":12},{"x":4,"y":5,"output":9}]
-        },
-        {
-            
-            title:"medium problem",
-            description:"some really long description here",
-            author:"phuc",
-            parameter:['x','y'],
-            output:[{"x":9,"y":3,"output":27},{"x":4,"y":5,"output":20}]
-        },
-        {
-            
-            title:"hard problem",
-            description:"some really long description here",
-            author:"phuc",
-            parameter:['x','y'],
-            output:[{"x":9,"y":3,"output":2},{"x":125,"y":5,"output":3}]
-        }
-    ];
-    return data[query.problem_id];
 },{query:z.object({
     problem_id:z.coerce.number()
-})});
+})}).use(auth_middleware).post("/like",async({user,query})=>{
+    if(user==null){throw status(401,"please login to like");}
+    return await get_connection(async(db)=>{
+        return await db.begin(async(db)=>{
+            let res=await db`insert into problem_info(problem_id,uid,status,reaction) values(${query.problem_id},
+            ${user.user_id},'none','liked') on conflict(problem_id,uid) do update set reaction='liked' 
+            where problem_info.problem_id=${query.problem_id} and problem_info.uid=${user.user_id} 
+            and problem_info.reaction!='liked' returning 1`;
+            if(!res.length){throw status(403,"you had already liked this post");}
+            res=await db`update problem set reaction=reaction+1 where id=${query.problem_id} returning 1`;
+            if(!res.length){
+                throw status(404,"problem not found");
+            }
+        });
+    });
+},{query:z.object({
+    problem_id:z.coerce.number()})
+}).post("/dislike",async({user,query})=>{
+    if(user==null){throw status(401,"please login to dislike");}
+    return await get_connection(async(db)=>{
+        return await db.begin(async(db)=>{
+            let res=await db`insert into problem_info(problem_id,uid,status,reaction) values(${query.problem_id},
+            ${user.user_id},'none','disliked') on conflict(problem_id,uid) do update set reaction='disliked' 
+            where problem_info.problem_id=${query.problem_id} and problem_info.uid=${user.user_id} 
+            and problem_info.reaction!='disliked' returning 1`;
+            if(!res.length){throw status(403,"you had already disliked this post");}
+            res=await db`update problem set reaction=reaction-1 where id=${query.problem_id} returning 1`;
+            if(!res.length){
+                throw status(404,"problem not found");
+            }
+        });
+    });
+},{query:z.object({
+    problem_id:z.coerce.number()})
+});
--- start.py
+++ start.py
@@ -2,7 +2,7 @@ import asyncpg
 import os
 import json
 async def run():
-    con:asyncpg.Connection=await asyncpg.connect(user="postgres")
+    con:asyncpg.Connection=await asyncpg.connect(user="postgres",password="postgres")
     async with con.transaction():
         #await con.execute("create type problem_difficulty as enum('easy','medium','hard')")
         #await con.execute('''
@@ -18,35 +18,14 @@ async def run():
                 #output JSONB
             #);
         #''')
-        await con.executemany('''insert into problem(title,author_id,
-                              author_name,description,difficulty,reaction,parameter,output) values($1,$2,$3,$4,$5,$6,$7,$8)''',
-                              [(
-                                    "easy problem",
-                                    "auth0|6a1d56b73668b52679f86a2f",
-                                    "phuc",
-                                    "a really long description here",
-                                    "easy",
-                                    3,
-                                    ['x','y']
-                                    ,json.dumps([{"x":9,"y":3,"output":12},{"x":4,"y":5,"output":9}])),
-                                (
-                                    "medium problem",
-                                    "auth0|6a1d56b73668b52679f86a2f",
-                                    "phuc",
-                                    "a really long description here",
-                                    "medium",
-                                    -13,
-                                    ['x','y'],
-                                    json.dumps([{"x":9,"y":3,"output":27},{"x":4,"y":5,"output":20}])),
-                                (
-                                    "hard problem",
-                                    "auth0|6a1d56b73668b52679f86a2f",
-                                    "phuc",
-                                    "a really long description here",
-                                    "hard",
-                                    26,
-                                    ['x','y'],
-                                    json.dumps([{"x":9,"y":3,"output":2},{"x":125,"y":5,"output":3}])
-                                )])
+        await con.execute("create type problem_status as enum('none','attempted','solved')")
+        await con.execute("create type user_reaction as enum('disliked','none','liked')")
+        await con.execute('''create table problem_info(
+                            uid text,
+                            problem_id integer,
+                            status problem_status,
+                            reaction user_reaction,
+                            primary key(problem_id,uid) 
+                          )''')
 import asyncio
 asyncio.run(run())
