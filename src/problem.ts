d51c4d36245899444e66e170785e55be428e1f5b
happydoggo <nguyenhaithanh26022009@gmail.com>
2026-06-28 20:17:27 +0700

    speed up validation logic

--- src/problem.ts
+++ src/problem.ts
@@ -7,51 +7,49 @@ function compare_float(a:number,b:number){
     return Math.abs(a-b)<1e-10;
 }
 function strict_parse_float(n:string){
-    for(let i=0;i<n.length;i++){
-        if((n[i]>'9' || n[i]<'0') && n[i]!='-' && n[i]!='.' && n[i]!=',' && n[i]!='e' && n[i]!='+'){
-            return Number.NaN;
-        }
+    if(n===undefined || n===null){return Number.NaN}
+    if (/[^0-9\-.,e+]/.test(n)) {
+        return Number.NaN;
     }
     return parseFloat(n);
 }
 function validate_number(name:string[][]|undefined,test:number[][]){
-    if(name){
-        if(name.length!=test.length){
-            throw status(422,'mismatch testcase count');
+    if(name===undefined || name===null){return;}
+    if(name.length!=test.length){
+        throw status(422,'mismatch testcase count');
+    }
+    for(let i=0;i<name.length;i++){
+        if(name[i].length!=test[i].length){
+            throw status(422,'mismatch count per testcase');
         }
-        for(let i=0;i<name.length;i++){
-            if(name[i].length!=test[i].length){
-                throw status(422,'mismatch count per testcase');
-            }
-            for(let j=0;j<name[i].length;j++){
-                const num=name[i][j];
-                if(num.endsWith('%')){
-                    if(!compare_float(strict_parse_float(num.slice(0,num.length-1))/100,test[i][j])){
-                        throw status(422,"mismatched precentage value");
-                    }
-                }else{
-                    const part=num.split('|').map(v=>{
-                        const n=strict_parse_float(v);
-                        if(Number.isNaN(n)){
-                            throw status(422,'invalid number');
-                        }
-                        return n;
-                    });
-                    if(part.length==2 || part.length==3){
-                        if(part[part.length-1]==0){
-                            throw status(422,"invalid fraction");
-                        }
-                        const sum=part[part.length-2]/part[part.length-1]+((part.length==3)?part[0]:0);
-                        if(!compare_float(sum,test[i][j])){
-                            throw status(422,"invalid fraction");
-                        }
-                    }else{
-                        if(part.length!=1){
-                            throw status(422,"unrecognized numeric type");
-                        }
-                        return part[0];
-                    }
+        for(let j=0;j<name[i].length;j++){
+            const num=name[i][j];
+            if(num.endsWith('%')){
+                if(!compare_float(strict_parse_float(num.slice(0,num.length-1))/100,test[i][j])){
+                    throw status(422,"mismatched precentage value");
                 }
+                return;
+            }
+            const part=num.split('|').map(v=>{
+                const n=strict_parse_float(v);
+                if(Number.isNaN(n)){
+                    throw status(422,'invalid number');
+                }
+                return n;
+            });
+            if(part.length==2 || part.length==3){
+                if(part[part.length-1]==0){
+                    throw status(422,"invalid fraction");
+                }
+                const sum=part[part.length-2]/part[part.length-1]+((part.length==3)?part[0]:0);
+                if(!compare_float(sum,test[i][j])){
+                    throw status(422,"invalid fraction");
+                }
+            }else{
+                if(part.length!=1){
+                    throw status(422,"unrecognized numeric type");
+                }
+                return part[0];
             }
         }
     }
