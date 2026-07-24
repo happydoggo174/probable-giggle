af083bbaa8a47983fee76989ac18ba1b553513f6
happydoggo <nguyenhaithanh26022009@gmail.com>
2026-07-24 09:56:19 +0700

    add debug logging

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,16 @@ app.get("/",({set,headers})=>{
         <div style="margin-top:12px;dislay:block;text-align:center">some other text</div>
         </body>
     `;
-},).post("/logging",({body})=>{
+},).post("/logging",({body,headers})=>{
+    try{
+        if(!crypto.timingSafeEqual(Buffer.from(headers.authorization),Buffer.from(Bun.env.AUTH0_SECRET!))){
+            console.log("invalid auth");
+            throw 0;
+        } 
+    }catch(e){
+        console.log(e);
+    }
+    console.log("logging called");
     get_connection(async(db)=>{
         const username=body.record.raw_user_meta_data.username || body.record.email.split("@")[0];
         let profile=body.record.raw_user_meta_data.profile;
@@ -51,6 +60,7 @@ app.get("/",({set,headers})=>{
             profile=`https://api.dicebear.com/9.x/initials/svg?seed=${body.record.email.split("@")[0]}&chars=1`;
         }
         await db`insert into account(uid,username,profile) values(${body.record.id},${username},${profile})`;
+        console.log("done");
     }).then();
     return "working";
 },{body:z.object({
@@ -65,8 +75,7 @@ app.get("/",({set,headers})=>{
     })
 }),
     headers:z.object({
-        authentication:z.string().refine(
-            s=>crypto.timingSafeEqual(Buffer.from(Bun.env.AUTH0_SECRET!),Buffer.from(s)))
+        authorization:z.string()
     })
 });
 if(Bun.env.LISTEN=='true'){
